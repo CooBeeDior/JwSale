a88b7e2205defe4b4379596089a68a78c6fff769
zhoupinghe <zhoupinghe@wiz.top>
2019-10-23 11:28:48 +0800

    1

--- JwSale.Model/Dto/Request/User/AddUser.cs
+++ JwSale.Model/Dto/Request/User/AddUser.cs
@@ -23,39 +23,34 @@ namespace JwSale.Model.Dto.Request.User
         public string Password { get; set; }
 
         /// <summary>
-        /// 真实姓名
-        /// </summary>
-        public string RealName { get; set; }
-
-
-        /// <summary>
         /// 手机号码
         /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; }
 
         /// <summary>
-        /// 省
+        /// 真实姓名
         /// </summary>
-        public string Province { get; set; }
+        public string RealName { get; set; }
         /// <summary>
-        /// 市
+        /// 性别 1：男 2：女
         /// </summary>
-        public string City { get; set; }
+        public int Sex { get; set; }
         /// <summary>
-        /// 区
+        /// 生日
         /// </summary>
-        public string Area { get; set; }
+        public DateTime? BirthDay { get; set; }
         /// <summary>
-        /// 详细地址
+        /// 身份证
         /// </summary>
-        public string Address { get; set; }
-
+        public string IdCard { get; set; }
         /// <summary>
-        /// 备注
+        /// 真实姓名拼音
         /// </summary>
-        public string Remark { get; set; }
-
-
+        public string RealNamePin { get; set; }
         /// <summary>
         /// qq号
         /// </summary>
@@ -72,25 +67,47 @@ namespace JwSale.Model.Dto.Request.User
         /// 职位名称
         /// </summary>
         public string PositionName { get; set; }
-
+        /// <summary>
+        /// 省
+        /// </summary>
+        public string Province { get; set; }
+        /// <summary>
+        /// 市
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// 区
+        /// </summary>
+        public string Area { get; set; }
+        /// <summary>
+        /// 详细地址
+        /// </summary>
+        public string Address { get; set; }
         /// <summary>
         /// 头像
         /// </summary>
         public string HeadImageUrl { get; set; }
+        /// <summary>
+        /// 状态：0：启用 1：停用
+        /// </summary>
+        public short Status { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
 
         /// <summary>
         /// 类型 1:端口授权  2:月租授权
         /// </summary>
         public int Type { get; set; }
-
         /// <summary>
         /// 微信号数量
         /// </summary>
         public int WxCount { get; set; }
+
         /// <summary>
         /// 过期时间
         /// </summary>
         public DateTime? ExpiredTime { get; set; }
-
     }
 }

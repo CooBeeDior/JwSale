39a4591d248eebf0c50ee583552e7aa914ea679b
zhoupinghe <zhoupinghe@wiz.top>
2019-11-19 21:31:03 +0800

    增加文件转16进制

--- JwSale.Api/Controllers/CommonController.cs
+++ JwSale.Api/Controllers/CommonController.cs
@@ -24,47 +24,91 @@ namespace JwSale.Api.Controllers
         {
 
         }
-        //[HttpPost("api/Common/GetHexBufferForm")]
-        //public ActionResult<ResponseBase> GetHexBufferForm([FromForm]IFormFile file)
-        //{
-        //    ResponseBase<IList<GetHexBufferResponse>> response = new ResponseBase<IList<GetHexBufferResponse>>();
-        //    if (file == null)
-        //    {
-        //        response.Success = false;
-        //        response.Message = "文件不能为空";
-        //    }
-        //    else
-        //    {
+        [HttpPost("api/Common/GetHexBufferFormToArray")]
+        public ActionResult<ResponseBase> GetHexBufferFormToArray([FromForm]IFormFile file)
+        {
+            ResponseBase<GetHexBufferListResponse> response = new ResponseBase<GetHexBufferListResponse>();
+            GetHexBufferListResponse getHexBufferListResponse = new GetHexBufferListResponse();
+            IList<GetHexBufferResponse> list = new List<GetHexBufferResponse>();
+            if (file == null)
+            {
+                response.Success = false;
+                response.Message = "文件不能为空";
+            }
+            else
+            {
+                var sm = file.OpenReadStream();
+                var buffer = sm.ToBuffer();            
+                int currentIndex = 0;
+                int size = 50000;
+                while (buffer.Length > currentIndex)
+                {
+                    GetHexBufferResponse getHexBufferResponse = new GetHexBufferResponse();
+                    getHexBufferResponse.Index = currentIndex;
+                    int currentSize = size;
+                    if (buffer.Length - currentIndex < size)
+                    {
+                        currentSize = buffer.Length - currentIndex;
+                    }
 
-        //        var sm = file.OpenReadStream();
-        //        var buffer = sm.ToBuffer();
+                 
+                    getHexBufferResponse.HexStr = buffer.Skip(currentIndex).Take(currentSize).ToArray().HexBufferToStr();
+                    getHexBufferResponse.Length = currentSize;
+                    list.Add(getHexBufferResponse);
+                    currentIndex += currentSize;
+                }
+                getHexBufferListResponse.List = list;
+                getHexBufferListResponse.TotalLength = buffer.Length;
+            }
 
-        //        IList<GetHexBufferResponse> list = new List<GetHexBufferResponse>();
-        //        int currentIndex = 0;
-        //        int size = 50000;
-        //        while (buffer.Length > currentIndex)
-        //        {
-        //            int currentSize = size;
-        //            if (buffer.Length - currentIndex < size)
-        //            {
-        //                currentSize = buffer.Length - currentIndex;
-        //            }
-
-        //            GetHexBufferResponse getHexBufferResponse = new GetHexBufferResponse();
-        //            getHexBufferResponse.HexStr = buffer.Skip(currentIndex).Take(currentSize).ToArray().HexBufferToStr();
-        //            getHexBufferResponse.Length = currentSize;
-        //            list.Add(getHexBufferResponse);
-        //            currentIndex += currentSize;
-        //        }
+            response.Data = getHexBufferListResponse;
+            return response;
+        }
 
 
-
-
-
-        //        response.Data = list;
-        //    }
-        //    return response;
-        //}
+        /// <summary>
+        /// 获取16进制字符串
+        /// </summary>
+        /// <param name="getHexBuffer"></param>
+        /// <returns></returns>
+        [HttpPost("api/Common/GetHexBufferToArray")]
+        public ActionResult<ResponseBase> GetHexBufferToArray(GetHexBufferRequest getHexBuffer)
+        {
+            ResponseBase<GetHexBufferListResponse> response = new ResponseBase<GetHexBufferListResponse>();
+            GetHexBufferListResponse getHexBufferListResponse = new GetHexBufferListResponse();
+            string base64 = null;
+            var base64Arr = getHexBuffer.Base64.Split(',');
+            if (base64Arr.Length == 2)
+            {
+                base64 = base64Arr[1];
+            }
+            else
+            {
+                base64 = getHexBuffer.Base64;
+            }
+            var buffer = Convert.FromBase64String(base64);
+            IList<GetHexBufferResponse> list = new List<GetHexBufferResponse>();
+            int currentIndex = 0;
+            int size = 50000;
+            while (buffer.Length > currentIndex)
+            {
+                GetHexBufferResponse getHexBufferResponse = new GetHexBufferResponse();
+                getHexBufferResponse.Index = currentIndex;
+                int currentSize = size;
+                if (buffer.Length - currentIndex < size)
+                {
+                    currentSize = buffer.Length - currentIndex;
+                } 
+                getHexBufferResponse.HexStr = buffer.Skip(currentIndex).Take(currentSize).ToArray().HexBufferToStr();
+                getHexBufferResponse.Length = currentSize;
+                list.Add(getHexBufferResponse);
+                currentIndex += currentSize;
+            }
+            getHexBufferListResponse.List = list;
+            getHexBufferListResponse.TotalLength = buffer.Length;
+            response.Data = getHexBufferListResponse;
+            return response;
+        }
         /// <summary>
         /// 获取16进制字符串(Form表单)
         /// </summary>
